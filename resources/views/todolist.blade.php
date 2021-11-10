c252a932825dc70362076d3f889757b6dddc0ec5
Lucas Teixeira <teixeiralucas94@gmail.com>
2021-11-10 14:15:14 -0300

    first commit

---
+++ resources/views/todolist.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>To-do-list</title>
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('css/app.css');}}"/> 
+    </head>
+    <body>
+        <header>
+            <div class="header">
+                <h3>to-do-list</h3>
+                <button class="add_task">Adicionar</button>
+            </div>
+        </header>
+        <div class="container">
+            
+        </div>
+        <footer></footer>
+    </body>
+</html>
